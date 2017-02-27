9e7ac7b40c583c2ee663382df93c663e5f520112
Eric Eakin <eseakin@gmail.com>
2017-02-26 18:51:20 -0800

    Added axios for http request to api. Added AlbumDetail and AlbumList.

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,17 @@
 
 // import a library to create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
 // create a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
